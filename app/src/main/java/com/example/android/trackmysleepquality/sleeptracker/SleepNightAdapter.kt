5079610d54ae2083ae5f128ef4d08942385782f3
Christopher Lucier <OneDudeDesign@gmail.com>
2020-12-29 06:30:44 -0800

    Step.06 Complete

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -44,19 +44,16 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater
-                .inflate(R.layout.list_item_sleep_night, parent, false)
 
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        // TODO (01) Refactor the logic for creating the ViewHolder into a function called from().
+    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView){
+        // done (01) Refactor the logic for creating the ViewHolder into a function called from().
 
-        // TODO (02) Make the from() function into a companion object.
+        // done (02) Make the from() function into a companion object.
 
-        // TODO (03) Move the companion object into the ViewHolder class, and have it
+        // done (03) Move the companion object into the ViewHolder class, and have it
         // return a ViewHolder.
 
         val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
@@ -78,6 +75,16 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
                 else -> R.drawable.ic_sleep_active
             })
         }
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val view = LayoutInflater.from(parent.context)
+                        .inflate(R.layout.list_item_sleep_night, parent, false)
+
+                return ViewHolder(view)
+            }
+        }
     }
+
+
 
 }
